009fa5756bf3c4732c9323808f366ea17f175ac6
Alphena <alphena@gmail.com>
2021-03-03 15:30:25 -0700

    updated js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+/*
+Global Variables
+*/
+
 var questions = [
     {
         ask: "What is Alphena's Cat's name?",
@@ -5,3 +9,29 @@ var questions = [
         correctAnswer: "Mew" 
     },
 ];
+
+
+//  var timer 
+
+/*
+Functions
+*/
+
+// Start Game
+    // timer starts 
+
+
+//target question text
+//set text to current 0
+//display array answers
+//
+
+
+
+/*
+Events
+*/
+
+/*
+Entry Points
+*/
