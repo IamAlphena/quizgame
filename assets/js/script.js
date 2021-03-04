a04e8cbcfcc092f307378438f6b5a782fff25384
Alphena <alphena@gmail.com>
2021-03-03 17:24:08 -0700

    expanded logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ Global Variables
 var questions = [
     {
         ask: "What is Alphena's Cat's name?",
-        answers: ["Ghoul", "Ondore", "Mew", "Tom"],
+        answers: ["Deku", "Ondore", "Mew", "Tom"],
         correctAnswer: "Mew" 
     },
 ];
@@ -17,20 +17,49 @@ var questions = [
 Functions
 */
 
-// Start Game
-    // timer starts 
+function startQuiz{
+
+    // display div class trivia
+
+    
+    
+    // timer starts
 
 
-//target question text
-//set text to current 0
-//display array answers
-//
+    //display array answers
+
+
+    //when answer is clicked, reveal next question
+
+    //if answer clicked !== correct answer 
+    //subtract time
+
+    //if timer === 0 end game
+
+    // if no more questions and timer > 0 end game
+
+    // record time remaining as score
+
+}
+
+//endGame 
+
+// display div class scorecard
+
+// show score of time remaining
+
+// record initials with score to local storage
+
+// show scores that have been saved. 
 
 
 
 /*
 Events
 */
+
+//when start is clicked run functon startQuiz
+
 
 /*
 Entry Points
