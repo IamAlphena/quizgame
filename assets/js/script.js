f6b5803edd4ea2ad770b1599d77e8acb1af9f0c2
Alphena <alphena@gmail.com>
2021-03-03 21:30:10 -0700

    timer and button working

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,23 +8,69 @@ var questions = [
         answers: ["Deku", "Ondore", "Mew", "Tom"],
         correctAnswer: "Mew" 
     },
+
+    {
+        ask: "What is the type of capitalization that Javascript uses?",
+        answers: ["Title Case","Camel Case", "Sentance Case", "Snake Case"],
+        correctAnswer: "Camel Case"
+    },
+    {
+        ask: "What is the way to comment on javascript?",
+        answers: ["//", "<!-- -.>", "#", " [ ] "],
+        correctAnswer: "//"
+    },
+    {
+        ask: "What is a media query used for in CSS?"
+        answers: ["Background Color","CSS If Statement","Lorem ipsum","Mobile Responsiveness"],
+        correctAnswer: "Mobile Responsiveness"
+    },
+    {
+        ask: "Which of these is JS not commonly used for?",
+        answers: ["Web Development", "Base Structure", "Backend", "Automation"],
+        correctAnswer: "Base Structure"
+    },
+    {
+        ask: "What does the div tag do?"
+        answers: ["Makes the code easier to read", "Changes the title of your page","Creates a defined space","Adds Lorem Ipsum to the page"],
+        correctAnswer: "Creates a defined space"
+    }
+
 ];
 
+var info = document.getElementsByClassName("info");
+var trivia = document.getElementsByClassName("trivia");
+var score = document.getElementsByClassName("scorecard");
+var btn = document.querySelector("#start");
+var timeSet = document.querySelector(".time");
 
-//  var timer 
+var timeLeft = 60;
 
 /*
 Functions
 */
 
-function startQuiz{
+function startQuiz(){
 
     // display div class trivia
 
-    
-    
+    trivia[0].style.display = "block";
+
+    // hide div info
+    info[0].style.display = "none" ;
+     
     // timer starts
 
+    function setTime(){
+    var timerInterval = setInterval(function(){
+        timeLeft--;
+        timeSet.textContent = "Time Remaining: " + timeLeft;
+
+        if (timeLeft === 0){
+        clearInterval(timerInterval);}
+    }, 1000);
+    }
+
+    setTime();
 
     //display array answers
 
@@ -59,6 +105,8 @@ Events
 */
 
 //when start is clicked run functon startQuiz
+
+btn.addEventListener('click', startQuiz);
 
 
 /*
