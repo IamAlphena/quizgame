10a94a3fc082d503dd108b3a564d1fc45c2e6ebd
Alphena <alphena@gmail.com>
2021-03-04 16:46:45 -0700

    askQuestion functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ Global Variables
 var questions = [
     {
         ask: "What is the type of capitalization that Javascript uses?",
-        answers: ["Title Case","Camel Case", "Sentance Case", "Snake Case"],
+        answers: ["Title Case", "Camel Case", "Sentance Case", "Snake Case"],
         correctAnswer: "Camel Case"
     },
     {
@@ -15,7 +15,7 @@ var questions = [
     },
     {
         ask: "What is a media query used for in CSS?",
-        answers: ["Background Color","CSS If Statement","Lorem ipsum","Mobile Responsiveness"],
+        answers: ["Background Color", "CSS If Statement", "Lorem ipsum", "Mobile Responsiveness"],
         correctAnswer: "Mobile Responsiveness"
     },
     {
@@ -25,7 +25,7 @@ var questions = [
     },
     {
         ask: "What does the div tag do?",
-        answers: ["Makes the code easier to read", "Changes the title of your page","Creates a defined space","Adds Lorem Ipsum to the page"],
+        answers: ["Makes the code easier to read", "Changes the title of your page", "Creates a defined space", "Adds Lorem Ipsum to the page"],
         correctAnswer: "Creates a defined space"
     }
 
@@ -47,21 +47,21 @@ var choices = "";
 Functions
 */
 
-function startQuiz(){
+function startQuiz() {
 
     // display div class trivia
 
     trivia[0].style.display = "block";
 
     // hide div info
-    info[0].style.display = "none" ;
-     
+    info[0].style.display = "none";
+
     // timer starts
     setTime();
 
     //display questions, with it's properties displayed on the page
     askQuestion();
- 
+
     //if answer clicked !== correct answer 
     //subtract time
 
@@ -73,44 +73,65 @@ function startQuiz(){
 
 }
 
-function setTime(){
-    var timerInterval = setInterval(function(){
+function setTime() {
+    var timerInterval = setInterval(function () {
         timeLeft--;
         timeSet.textContent = "Time Remaining: " + timeLeft;
 
-        if (timeLeft === 0){
-        clearInterval(timerInterval);}
+        if (timeLeft === 0) {
+            clearInterval(timerInterval);
+        }
     }, 1000);
+}
+
+function askQuestion() {
+
+// if statement able to end quiz
+    if (timeLeft === 0 || currentQuestion === questions.length) {
+        endGame();
+        return;
     }
 
-function askQuestion(){
-        
-        
-    var triviaQuestion = questions[currentQuestion];
-    currentAsk.textContent = triviaQuestion.ask;
-        
-    // if (currentQuestion < questions.length) {   
-    //     currentAsk.textContent = questions[currentQuestion].ask;}
+var triviaQuestion = questions[currentQuestion];
+currentAsk.textContent = triviaQuestion.ask;
 
-    //populate array choices with questions[currentQuestion].answers
-    choices = triviaQuestion.answers;
-    console.log(choices)
-
-    choices.forEach(function (item) {
-        var choiceButton = document.createElement("button");
-        choiceButton.textContent = item;
-        answerChoices.append(choiceButton);
-    });
-
-    
-    //turn array choices into buttons inside answerChoices
-    
+//populate array choices with questions[currentQuestion].answers
+choices = triviaQuestion.answers;
+console.log(choices)
+//turn array choices into buttons inside answerChoices
+choices.forEach(function (item) {
+    var choiceButton = document.createElement("button");
+    choiceButton.textContent = item;
+    choiceButton.id = item;
+    answerChoices.append(choiceButton);
     // hit button, the next ask in array questions is shown
+    choiceButton.addEventListener('click', e => {
+        console.log(e.target.id)
+        // check if button id === correctAnswer
+        //if !== then timeLeft--5
+        if (e.target.id !== triviaQuestion.correctAnswer) {
+            console.log("wrong");
+            timeLeft -= 5;
+        }
+        // next question
+        currentQuestion++;
+        answerChoices.innerHTML = "";
+        askQuestion();
+    })
 
-    //when answer is clicked, reveal next question
-    
+
+});
     }
-//endGame 
+
+
+function endGame() {
+    // display div class scorecard
+
+    score[0].style.display = "block";
+
+    // hide div trivia
+    trivia[0].style.display = "none";
+}
 
 // display div class scorecard
 
@@ -129,7 +150,6 @@ Events
 //when start is clicked run functon startQuiz
 
 btn.addEventListener('click', startQuiz);
-
 
 /*
 Entry Points
